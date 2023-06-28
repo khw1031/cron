5f1f8e98fb9cf4f10b2a6e72ee96ae8deb6c569c
khw1031 <khw1031@gmail.com>
2023-06-28 15:38:12 +0900

    axios -> fetch

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,25 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
-import axios from "axios";
-
 export const config = {
   runtime: "edge",
 };
 
 export default () => {
-  
-  axios(
+  fetch(
     "https://wh.jandi.com/connect-api/webhook/24103837/8b387069bc2751bceb53d12c9a9b122c",
     {
       method: "POST",
-      data: {
+      body: JSON.stringify({
         body: "🕰️ 주간업무보고 작성 리마인더 🕰️",
         connectColor: "#087f5b",
         connectInfo: [
           {
-            title: "👉 [주간 업무 보고 작성하러 가기](https://hanssem.atlassian.net/wiki/spaces/FIST/pages/21692493/2.2)",
+            title:
+              "👉 [주간 업무 보고 작성하러 가기](https://hanssem.atlassian.net/wiki/spaces/FIST/pages/21692493/2.2)",
           },
         ],
-      },
+      }),
     }
   );
 };
